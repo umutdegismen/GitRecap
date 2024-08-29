7d7e2bb14bbcd43b4fa48a5b7a91216b235cdf02
umutdegismen <umutdegismen@gmail.com>
2024-08-29 18:07:21 -0400

    first commit

---
+++ src/com/github/practice1/practice1.java
@@ -0,0 +1,14 @@
+package com.github.practice1;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+		// command 1
+		// command 2
+		
+
+	}
+}
